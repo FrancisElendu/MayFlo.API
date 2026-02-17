839b78016f789bf5c0ed6dc95220b6dfeba19af4
Francis Elendu <elcfran6@hotmail.com>
2026-02-17 00:53:59 -0600

    added retry to db connection

--- MayFlo.API/Program.cs
+++ MayFlo.API/Program.cs
@@ -1,10 +1,11 @@
 using MayFlo.API.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.OpenApi.Models;
 using MSSQLFlexCrud.DatatContext;
 using MSSQLFlexCrud.Repositories;
 using MSSQLFlexCrud.SqlDb;
-using Microsoft.OpenApi.Models;
-using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,7 +18,15 @@ builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<BlogDbContext>(options =>
     options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")));
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        sqlOptions =>
+        {
+            sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 3,
+            maxRetryDelay: TimeSpan.FromSeconds(10),
+            errorNumbersToAdd: null);
+
+        }));
 
 builder.Services.AddScoped<AppDbContext>(provider =>
     provider.GetRequiredService<BlogDbContext>());
